950f8a2190b2795e87b0af72175c908597044fe5
oharar <bohara@senckenberg.de>
2018-12-05 15:51:57 +0100

    Start things off, and add SimOcc function

---
+++ R/SimOcc.R
@@ -0,0 +1,20 @@
+# Function to simulate simple occupancy
+#  Inputs:
+#    PrOcc - Probability of occupancy. If a scalar, all sites have same probability.
+#            If a vector, simulates as many sites as length of PrOcc, with different
+#            probabilities
+#    PrObs - Probability of observation. If a scalar, all sites have same probability.
+#            If a vector, simulates as many sites as length of PrOcc, with different
+#            probabilities
+#  NVisits - number of visits. Defaults to 4
+#   NPlots - number of plots.  Ignored if length(PrOcc) > 1. Defaults to 100
+SimOcc <- function(PrOcc, PrObs, NVisits = 4, NPlots = 100) {
+  if(length(PrOcc)!=length(PrObs)) stop("PrOcc and PrObs should be same length")
+  if(length(PrOcc)==1) PrOcc <- rep(PrOcc, NPlots)
+  if(length(PrObs)==1) PrObs <- rep(PrObs, NPlots)
+  if(length(NVisits)==1) NVisits <- rep(NVisits, NPlots)
+  
+  Occ <- rbinom(length(PrOcc), 1, PrOcc)
+  Obs <- rbinom(length(PrOcc), NVisits, PrObs)*Occ
+  cbind(Occupancy=Occ, Observations=Obs, Visits=NVisits)
+}
