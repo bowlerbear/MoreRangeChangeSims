f123d95964595a811cc45cb946486d844b1f5bdb
Bowler <db40fysa@idiv.de>
2019-06-03 14:50:12 +0200

    renaming the file

--- R/plotOccupancyHistory.R
+++
@@ -1,8 +0,0 @@
-plotOccupancyHistory <- function(obs){
-  require(ggplot)
-  ggplot(obs)+
-    geom_point(aes(x=Visit,y=Species,color=factor(obs)),size=2)+
-    scale_colour_manual(values=c("black","white"))+
-    theme(legend.position="none")+
-    facet_wrap(~Site)
-}
