9aa3900851451440cb3b039c20cd2f5c4e5bc2bc
Bowler <db40fysa@idiv.de>
2019-06-12 12:10:12 +0200

    Centering listlength and year

--- R/getBugsData.R
+++ R/getBugsData.R
@@ -2,10 +2,11 @@ getBugsData <- function(modelData = modelData){
   
   bugs.data <- list(nyear = length(unique(modelData$Year)),
                     nsite = length(unique(modelData$Site)),
+                    Year = sort(unique(modelData$Year))-mean(unique(modelData$Year)),
                     year = modelData$Year,
                     site = modelData$Site,
                     y = modelData$Obs,
-                    L = log(modelData$L),
+                    L = log(modelData$L)-mean(log(modelData$L)),
                     nvisit = nrow(modelData))
   
   return(bugs.data)
