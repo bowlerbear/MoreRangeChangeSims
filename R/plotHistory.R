4a006166e7e03404e3f29391f8f51e839588804a
Bowler <db40fysa@idiv.de>
2019-08-27 18:57:52 +0200

    Reorganizing file structure

--- R/plotHistory.R
+++
@@ -1,8 +0,0 @@
-plotHistory <- function(obs){
-  require(ggplot2)
-  ggplot(obs)+
-    geom_point(aes(x=Visit,y=Species,color=factor(obs)),size=2)+
-    scale_colour_manual(values=c("white","black"))+
-    theme(legend.position="none")+
-    facet_wrap(~Site)
-}
