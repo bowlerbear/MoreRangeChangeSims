c5af91d4b136f9c77522f6acd0f399e06a956f77
Bowler <db40fysa@idiv.de>
2019-09-27 09:56:46 +0200

    Function to take input parameter csv file and output the parmeters into the general environment

---
+++ R/getParams.R
@@ -0,0 +1,70 @@
+getParams <- function(parameterDF,mysimNu){
+  
+  temp <- subset(parameterDF,SimNu==mysimNu) 
+  
+  #Number of sites in the landscape
+  NSites <<- temp$Nsites
+  
+  #Number of species in the regional pool
+  NSpecies <<- temp$Nspecies 
+  
+  #Number of time points
+  NYears <<- temp$Nyears
+  
+  #Define average habitat quality of each site
+  Scovariate <<- rnorm(NSites,0,temp$ScovariateVar)
+  
+  #Define time trend
+  Tcovariate <<- (1:NYears)-mean(1:NYears)
+  
+  #sampling type
+  samplingType <<- temp$samplingType
+  
+  #Define species responses
+  speciesVariation <<- temp$speciesVariation
+  
+  if(speciesVariation==TRUE){
+    OccProb <<- rnorm(NSpecies,temp$meanOccProb,temp$meanOccProb/3)#occupancy
+    lambda <<- rlnorm(NSpecies,temp$meanLambda,temp$meanLambda/2)#abundance
+    Seffects <<- rnorm(NSpecies,temp$meanSeffects,temp$meanSeffects/5)##Define spatial quality effects
+    Teffects <<- rnorm(NSpecies,temp$meanTeffects,temp$meanTeffects/5)#Effect of year
+    Ieffects <<- rnorm(NSpecies,temp$meanIeffects,temp$meanIeffects/5)#Effect of year
+  } else{
+    OccProb <<- rep(temp$meanOccProb,NSpecies)
+    lambda <<- rep(temp$meanLambda, NSpecies)
+    Seffects <<- rep(temp$meanSeffects,NSpecies)
+    Teffects <<- rep(temp$meanTeffects,NSpecies)
+    Ieffects <<- rep(temp$meanIeffects,NSpecies)
+  }
+  
+  #Define sampling effects
+  
+  #Number of visits to each site within each year
+  NVisits <<- temp$Nvisits
+  
+  #are we subsetting to only positive results
+  subsetPositive <<- temp$subsetPositive
+  
+  #Define average detection probability
+  if(speciesVariation==TRUE){
+    DetProb <<- rnorm(NSpecies,temp$meanDetProb,temp$meanDetProb/3) 
+    SiteDetEffects <<- rnorm(NSpecies,temp$meanSiteDetEffects,temp$meanSiteDetEffects/5)
+    YearDetEffects <<- rnorm(NSpecies,temp$meanYearDetEffects,temp$meanYearDetEffects/5)
+    IntDetEffects <<- rnorm(NSpecies,temp$meanIntDetEffects,temp$meanIntDetEffects/5)
+  }else{ 
+    DetProb <<- rep(temp$meanDetProb,NSpecies)
+    SiteDetEffects <<- rep(temp$meanSiteDetEffects,NSpecies)
+    YearDetEffects <<- rep(temp$meanSiteDetEffects,NSpecies)
+    IntDetEffects <<- rep(temp$meanSiteDetEffects,NSpecies)
+  }
+  
+  #site/year detection noise (observer variation in effort)
+  Noise <<- temp$Noise
+  
+  #Identify focal species
+  focalSpecies <<- temp$focalSpecies
+  
+  #model type
+  myModel <<- temp$myModel
+  
+}
